5249517648fa22ac6010d85e884f969a9e7a8bc9
teklynk <ryanjones153@gmail.com>
2022-01-01 04:49:33 -0500

    clips is working with slider!

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -11,6 +11,20 @@ $(document).ready(function () {
     let channelMessage = getUrlParameter('msg');
     let timeOut = getUrlParameter('timeOut');
     let modsOnly = getUrlParameter('modsOnly');
+    let useClips = getUrlParameter('useClips');
+
+
+    if (!timeOut) {
+        timeOut = 10; // default
+    }
+
+    if (!useClips) {
+        useClips = 'false'; // default
+    }
+
+    if (!modsOnly) {
+        modsOnly = 'true'; // default
+    }
 
     // Twitch API get user info for !so command
     let getUserInfo = function (SOChannel, callback) {
@@ -75,28 +89,55 @@ $(document).ready(function () {
                     // Timeout start
                     let timer = 0;
 
-                    // Remove video after timeout has been reached
+                    // TODO: Refactor this logic. A lot of duplicate code here.
+
+                    // Remove slider elements after timeout has been reached
                     let startTimer = setInterval(function () {
                         timer++; // Increment timer
 
-                        console.log(timer);
-
-                        if (timer === parseInt(timeOut)) {
+                        if (timer > 1 && useClips === 'true' && localStorage.getItem('TwitchSOVideoState') !== 'active') {
+                            // Slide in animation
                             document.getElementById("userMsg").classList.remove("slide-left-in");
                             document.getElementById("userImage").getElementsByClassName("image")[0].classList.remove("fade-in-image");
                             document.getElementById("userName").classList.remove("slide-right-in");
 
+                            // Slide out animation
                             document.getElementById("userMsg").classList.add("slide-right-out");
                             document.getElementById("userImage").getElementsByClassName("image")[0].classList.add("fade-out-image");
                             document.getElementById("userName").classList.add("slide-left-out");
-                            timer = 0; // reset timer to zero
-                            clearInterval(startTimer);
 
-                            // Completely remove elements after 3 seconds
+                            // Completely remove elements after 1 seconds and animation has completed
                             setTimeout(function () {
-                                document.getElementById("userMsg").remove();
-                                document.getElementById("userImage").remove();
-                                document.getElementById("userName").remove();
+                                if (document.getElementById("userMsg") || document.getElementById("userImage") || document.getElementById("userName")) {
+                                    document.getElementById("userMsg").remove();
+                                    document.getElementById("userImage").remove();
+                                    document.getElementById("userName").remove();
+                                }
+                                timer = 0; // Reset timer to zero
+                                clearInterval(startTimer);
+                            }, 1000);
+                        }
+
+                        if (useClips === 'false' && timer === parseInt(timeOut)) {
+                            // Slide in animation
+                            document.getElementById("userMsg").classList.remove("slide-left-in");
+                            document.getElementById("userImage").getElementsByClassName("image")[0].classList.remove("fade-in-image");
+                            document.getElementById("userName").classList.remove("slide-right-in");
+
+                            // Slide out animation
+                            document.getElementById("userMsg").classList.add("slide-right-out");
+                            document.getElementById("userImage").getElementsByClassName("image")[0].classList.add("fade-out-image");
+                            document.getElementById("userName").classList.add("slide-left-out");
+
+                            // Completely remove elements after 1 seconds and animation has completed
+                            setTimeout(function () {
+                                if (document.getElementById("userMsg") || document.getElementById("userImage") || document.getElementById("userName")) {
+                                    document.getElementById("userMsg").remove();
+                                    document.getElementById("userImage").remove();
+                                    document.getElementById("userName").remove();
+                                }
+                                timer = 0; // Reset timer to zero
+                                clearInterval(startTimer);
                             }, 1000);
                         }
 
