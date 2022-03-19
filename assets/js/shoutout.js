62b70d5bfaeb540553aed0cc4d071d4be3bdb19e
teklynk <ryanjones153@gmail.com>
2022-03-18 22:22:12 -0400

    WIP

--- assets/js/shoutout.js
+++ assets/js/shoutout.js
@@ -20,11 +20,13 @@ $(document).ready(function () {
     // URL values
     let mainAccount = getUrlParameter('channel').toLowerCase().trim();
     let limit = getUrlParameter('limit').trim();
-    let shuffle = "true";
     let showText = getUrlParameter('showText').trim();
     let customTitle = getUrlParameter('customTitle').trim();
     let command = getUrlParameter('command').trim();
     let showMsg = getUrlParameter('showMsg').trim();
+    let showClip = getUrlParameter('showClip');
+    let showRecentClip = getUrlParameter('showRecentClip');
+    let showImage = getUrlParameter('showImage');
     let ref = getUrlParameter('ref').trim();
     let customMsg = getUrlParameter('customMsg').trim();
     let timeOut = getUrlParameter('timeOut');
@@ -36,6 +38,8 @@ $(document).ready(function () {
     let curr_clip = '';
     let titleText = '';
     let timer = 0;
+    let showImageTimer = 0;
+    let startImageTimer = 0;
     let startTimer;
 
     if (!modsOnly) {
@@ -66,129 +70,93 @@ $(document).ready(function () {
         alert('Twitch access token not set');
     }
 
-    // If shoutout and Auth token set, then connect to chat using oauth.
-    if (showMsg === 'true' && ref) {
-
+    // If Auth token is set, then connect to chat using oauth, else connect anonymously.
+    if (ref) {
         client = new tmi.Client({
             options: {
                 debug: true,
                 skipUpdatingEmotesets: true
             },
-            connection: {
-                reconnect: true,
-                secure: true,
-            },
+            connection: {reconnect: true},
             identity: {
                 username: mainAccount,
                 password: 'oauth:' + atob(ref)
             },
             channels: [mainAccount]
         });
-
-        client.connect().catch(console.error);
-
-        // triggers on message
-        client.on('chat', (channel, user, message, self) => {
-
-            // Ignore echoed messages.
-            if (self) {
-                return false;
-            }
-
-            // Clean message
-            chatmessage = message.replace(/(<([^>]+)>)/ig, "").trim();
-
-            // alert message
-            if (user['message-type'] === 'chat') {
-
-                if (chatmessage.startsWith("!" + command)) {
-
-                    // Remove characters and spaces. Lowercase the string
-                    chatmessage = chatmessage.replace('@', '').trim().toLowerCase();
-
-                    // Convert string to an array/list - split on spaces
-                    // Skip first item in array because the first item is the chat command and not a channel name
-                    chatmessage = chatmessage.split(' ').slice(1).filter(String);
-
-                    // Ignore chat command if already playing a video
-                    if ($('video').length) {
-
-                        console.log('Can not shout-out while a clip is playing!');
-                        return false;
-
-                    } else {
-                        // Set clip index to 1 if only doing 1 shout-out else force start at index 0
-                        if (clip_index === chatmessage.length) {
-                            clip_index = 1;
-                        } else {
-                            clip_index = 0;
-                        }
-
-                        if (modsOnly === 'true' && (user.mod || user.username === mainAccount)) {
-                            loadClip(chatmessage[0]);
-                        } else if (modsOnly === 'false' || user.username === mainAccount) {
-                            loadClip(chatmessage[0]);
-                        }
-                    }
-
-                } else {
-
-                    return false;
-                }
-
-            }
+    } else {
+        client = new tmi.Client({
+            options: {
+                debug: true,
+                skipUpdatingEmotesets: true
+            },
+            connection: {reconnect: true},
+            channels: [mainAccount]
         });
     }
 
-    // Get and play the clip
-    function loadClip(channelName) {
+    client.connect().catch(console.error);
 
-        // Set clip index to 1 if only doing 1 shout-out else increment index
-        if (clip_index === chatmessage.length) {
-            clip_index = 1;
-        } else {
-            clip_index++;
-        }
+    // triggers on message
+    client.on('chat', (channel, user, message, self) => {
 
-        // Debug
-        console.log('clip_index loadClip: ' + clip_index);
-        console.log('message length loadClip: ' + chatmessage.length);
-
-        // Json data - Ajax call
-        let clips_json = JSON.parse($.getJSON({
-            'url': "https://twitchapi.teklynk.com/getuserclips.php?channel=" + channelName + "&limit=" + limit + "",
-            'async': false
-        }).responseText);
-
-        // If no user clips exist, then skip to the next channel
-        if (!clips_json.data || typeof clips_json.data === 'undefined' || clips_json.data.length === 0) {
-            console.log('no clips exist for channel: ' + channelName);
-            nextClip();
+        // Ignore echoed messages.
+        if (self) {
             return false;
         }
 
-        // Sort array by created_at
-        clips_json.data.sort(sortByProperty('created_at'));
+        // Clean message
+        chatmessage = message.replace(/(<([^>]+)>)/ig, "").trim();
 
-        // Grab a random clip index anywhere from 1 to the limit value. Else, grab the most recent popular clip.
-        if (shuffle === 'true') {
-            randomClip = Math.floor((Math.random() * clips_json.data.length - 1) + 1);
+        // alert message
+        if (user['message-type'] === 'chat' && chatmessage.startsWith("!" + command)) {
+
+            // Remove characters and spaces. Lowercase the string
+            chatmessage = chatmessage.replace('@', '').trim().toLowerCase();
+
+            // Convert string to an array/list - split on spaces
+            // Skip first item in array because the first item is the chat command and not a channel name
+            chatmessage = chatmessage.split(' ').slice(1).filter(String);
+
+            // Ignore chat command if already playing a video
+            if ($('video').length) {
+
+                console.log('Can not shout-out while a clip is playing!');
+                return false;
+
+            } else {
+                // Set clip index to 1 if only doing 1 shout-out else force start at index 0
+                if (clip_index === chatmessage.length) {
+                    clip_index = 1;
+                } else {
+                    clip_index = 0;
+                }
+
+                if (modsOnly === 'true' && (user.mod || user.username === mainAccount)) {
+                    loadClip(chatmessage[0]);
+                } else if (modsOnly === 'false' || user.username === mainAccount) {
+                    loadClip(chatmessage[0]);
+                }
+            }
+
         } else {
-            randomClip = 0;
+
+            return false;
         }
 
-        // Remove old video element and text - resets everything
-        document.getElementById('container').innerHTML = "";
+    });
 
+    // Custom title
+    function Title(name) {
         // Text on top of clip
         if (showText === 'true') {
             if (customTitle) {
                 customTitle = getUrlParameter('customTitle').trim();
-                customTitle = customTitle.replace("{channel}", clips_json.data[0]['broadcaster_name']);
-                customTitle = customTitle.replace("{url}", "twitch.tv/" + clips_json.data[0]['broadcaster_name'].toLowerCase());
+                customTitle = customTitle.replace("{channel}", name);
+                customTitle = customTitle.replace("{url}", "twitch.tv/" + name.toLowerCase());
                 titleText = "<div id='text-container'><span class='title-text'>" + customTitle + "</span></div>";
             } else {
-                titleText = "<div id='text-container'><span class='title-text'>Go check out " + clips_json.data[0]['broadcaster_name'] + "</span></div>"
+                titleText = "<div id='text-container'><span class='title-text'>Go check out " + name + "</span></div>"
             }
 
             $(titleText).appendTo('#container');
@@ -196,54 +164,160 @@ $(document).ready(function () {
         } else {
             titleText = '';
         }
+    }
 
-        // Create new video element
-        curr_clip = document.createElement('video');
-        // Append video element to the container div
-        $(curr_clip).appendTo('#container');
+    // Get and play the clip
+    function loadClip(channelName) {
 
-        // Parse thumbnail image to build the clip url
-        let thumbPart = clips_json.data[randomClip]['thumbnail_url'].split("-preview-");
-        thumbPart = thumbPart[0] + ".mp4";
+        let user_json = JSON.parse($.getJSON({
+            'url': "https://twitchapi.teklynk.com/getuserinfo.php?channel=" + channelName + "",
+            'async': false
+        }).responseText);
 
-        // Load a new clip
-        curr_clip.src = thumbPart;
-        curr_clip.autoplay = true;
-        curr_clip.controls = false;
-        curr_clip.volume = 1.0;
-        curr_clip.load();
+        if (user_json.data.length === 0) {
+            nextClip();
+            return false;
+        }
 
-        // Debug
-        console.log('channelName: ' + channelName);
-        console.log('clipNumber: ' + randomClip);
+        // Show Clip
+        if (showClip === 'true' || showRecentClip === 'true') {
 
-        // Remove video after timeout has been reached and then play next clip
-        startTimer = setInterval(function () {
-            timer++; // Increment timer
-
-            console.log(timer);
-
-            if (timer === parseInt(timeOut)) {
-                // Remove existing video element
-                if ($('video').length) {
-                    $('video').remove();
-                }
-
-                if ($('#text-container').length) {
-                    $('#text-container').remove();
-                }
-
-                timer = 0; // reset timer to zero
-                clearInterval(startTimer);
-
-                nextClip();
+            // Set clip index to 1 if only doing 1 shout-out else increment index
+            if (clip_index === chatmessage.length) {
+                clip_index = 1;
+            } else {
+                clip_index++;
             }
 
-        }, 1000);
+            // Debug
+            console.log('clip_index loadClip: ' + clip_index);
+            console.log('message length loadClip: ' + chatmessage.length);
 
-        // Move to the next clip if the current one finishes playing
-        curr_clip.addEventListener("ended", nextClip);
+            // Json data - Ajax call
+            let clips_json = JSON.parse($.getJSON({
+                'url': "https://twitchapi.teklynk.com/getuserclips.php?channel=" + channelName + "&limit=" + limit + "",
+                'async': false
+            }).responseText);
 
+            if (clips_json.data.length === 0) {
+                nextClip();
+                return false;
+            }
+
+            // If no user clips exist, then skip to the next channel
+            if (!clips_json.data || typeof clips_json.data === 'undefined' || clips_json.data.length === 0) {
+
+                console.log('no clips exist for channel: ' + channelName);
+
+                // Do a shout-out message if enabled
+                shoutOutMsg(channelName);
+
+                if (showImage === 'true') {
+
+                    console.log('show user profile image');
+
+                    // Remove video element after all shout-outs have ended
+                    document.getElementById('container').innerHTML = "";
+
+                    Title(user_json.data[0]['display_name']);
+
+                    $(titleText + "<img id='profile' class='fade img-fluid' src='" + user_json.data[0]['profile_image_url'] + "'>").appendTo("#container");
+
+                    startImageTimer = setInterval(function () {
+
+                        showImageTimer++; // Increment timer
+                        console.log(timer);
+
+                        if (showImageTimer === 10) {
+
+                            showImageTimer = 0; // reset timer to zero
+                            clearInterval(startImageTimer);
+
+                            console.log('removing profile image');
+
+                            $("img").remove();
+
+                            nextClip();
+
+                        }
+
+                    }, 1000);
+
+                } else {
+                    nextClip();
+                }
+            }
+
+            // Sort array by created_at
+            clips_json.data.sort(sortByProperty('created_at'));
+
+            // Grab a random clip index anywhere from 1 to the limit value. Else, grab the most recent popular clip.
+            if (showClip === 'true') {
+                randomClip = Math.floor((Math.random() * clips_json.data.length - 1) + 1);
+            } else {
+                randomClip = 0;
+            }
+
+            // Remove old video element and text - resets everything
+            document.getElementById('container').innerHTML = "";
+
+            // Text on top of clip
+            Title(clips_json.data[0]['broadcaster_name']);
+
+            // Create new video element
+            curr_clip = document.createElement('video');
+            // Append video element to the container div
+            $(curr_clip).appendTo('#container');
+
+            // Parse thumbnail image to build the clip url
+            let thumbPart = clips_json.data[randomClip]['thumbnail_url'].split("-preview-");
+            thumbPart = thumbPart[0] + ".mp4";
+
+            // Load a new clip
+            curr_clip.src = thumbPart;
+            curr_clip.autoplay = true;
+            curr_clip.controls = false;
+            curr_clip.volume = 1.0;
+            curr_clip.load();
+
+            // Debug
+            console.log('channelName: ' + channelName);
+            console.log('clipNumber: ' + randomClip);
+
+            // Remove video after timeout has been reached and then play next clip
+            startTimer = setInterval(function () {
+                timer++; // Increment timer
+
+                console.log(timer);
+
+                if (timer === parseInt(timeOut)) {
+                    // Remove existing video element
+                    if ($('video').length) {
+                        $('video').remove();
+                    }
+
+                    if ($('#text-container').length) {
+                        $('#text-container').remove();
+                    }
+
+                    timer = 0; // reset timer to zero
+                    clearInterval(startTimer);
+
+                    nextClip();
+                }
+
+            }, 1000);
+
+            // Move to the next clip if the current one finishes playing
+            curr_clip.addEventListener("ended", nextClip);
+        }
+
+        // Do a shout-out message if enabled
+        shoutOutMsg(channelName);
+
+    }
+
+    function shoutOutMsg(channelName) {
         // Do a shout-out for each clip
         if (showMsg === 'true' && ref) {
             let so_json = JSON.parse($.getJSON({
@@ -251,21 +325,24 @@ $(document).ready(function () {
                 'async': false
             }).responseText);
 
-            // Custom message. Replace {variable} with actual values
-            if (customMsg) {
-                customMsg = getUrlParameter('customMsg').trim();
-                customMsg = customMsg.replace('{channel}', so_json.data[0]['broadcaster_name']);
-                customMsg = customMsg.replace('{game}', so_json.data[0]['game_name']);
-                customMsg = customMsg.replace('{title}', so_json.data[0]['title']);
-                customMsg = customMsg.replace('{url}', "https://twitch.tv/" + so_json.data[0]['broadcaster_login']);
-                // Say custom message
-                client.say(mainAccount, customMsg);
-            } else {
-                // Say default message
-                client.say(mainAccount, "Go check out " + so_json.data[0]['broadcaster_name'] + "! They were playing: " + so_json.data[0]['game_name'] + " - " + so_json.data[0]['title'] + " - https://twitch.tv/" + so_json.data[0]['broadcaster_login']);
+            // Check if user has streamed anything before doing a shout-out
+            if (so_json.data && so_json.data[0]['title']) {
+                // Custom message. Replace {variable} with actual values
+                if (customMsg) {
+                    customMsg = getUrlParameter('customMsg').trim();
+                    customMsg = customMsg.replace('{channel}', so_json.data[0]['broadcaster_name']);
+                    customMsg = customMsg.replace('{game}', so_json.data[0]['game_name']);
+                    customMsg = customMsg.replace('{title}', so_json.data[0]['title']);
+                    customMsg = customMsg.replace('{url}', "https://twitch.tv/" + so_json.data[0]['broadcaster_login']);
+                    // Say custom message
+                    client.say(mainAccount, customMsg);
+                } else {
+                    // Say default message
+                    client.say(mainAccount, "Go check out " + so_json.data[0]['broadcaster_name'] + "! They were playing: " + so_json.data[0]['game_name'] + " - " + so_json.data[0]['title'] + " - https://twitch.tv/" + so_json.data[0]['broadcaster_login']);
+                }
             }
-        }
 
+        }
     }
 
     function nextClip() {
@@ -282,7 +359,7 @@ $(document).ready(function () {
 
         console.log('Play The Next Clip!');
 
-        loadClip(chatmessage[clip_index]);
+        loadClip(chatmessage[clip_index].replace('@', '').trim().toLowerCase());
 
         curr_clip.play();
     }
